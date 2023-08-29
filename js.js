67b6a40db91c30bbe2fee3aa3c74bd38d9a12c61
Arturo V <jsbach03@live.com.mx>
2023-08-28 22:01:49 -0600

    Final 22 horas

--- js.js
+++ js.js
@@ -1,83 +1,110 @@
+let pokemonActual;
+
 let botonMostrarPokemonAleatorio = document.getElementById("botonMostrarPokemonAleatorio");
-botonMostrarPokemonAleatorio.addEventListener("click", mostrarPokemon);
+botonMostrarPokemonAleatorio.addEventListener("click", consultarPokemon);
 
-let botonMostrarDetalles = document.getElementById("botonMostrarDetalles");
-botonMostrarDetalles.addEventListener("click", mostrarDetalles);
+let botonAgregarFavoritos = document.getElementById("botonAgregarFavoritos");
+botonAgregarFavoritos.addEventListener("click", agregarAFavoritos);
 
-let botonOcultarDetalles = document.getElementById("botonOcultarDetalles");
-botonOcultarDetalles.addEventListener("click", ocultarDetalles);
-
-let botonAgregar = document.getElementById("botonAgregar");
-botonAgregar.addEventListener("click", agregarAFavoritos);
-
-function mostrarPokemon(){
+function consultarPokemon(){
     let id = Math.floor(Math.random() * 151);
-    consultarPokemon(id);
-}
 
-function consultarPokemon(id){
-    // Creamos un nuevo XMLHttpRequest
     var xhttp = new XMLHttpRequest();
-
-    // Esta es la función que se ejecutará al finalizar la llamada
+    
     xhttp.onreadystatechange = function() {
-    // Si nada da error
-    if (this.readyState == 4 && this.status == 200) {
-        // La respuesta, aunque sea JSON, viene en formato texto, por lo que tendremos que hace run parse
-        let resultado = JSON.parse(this.responseText);
-        mostrar(resultado);
-        console.log(resultado);
-    }
+        if (this.readyState == 4 && this.status == 200) {
+            let pokemon = JSON.parse(this.responseText);
+            pokemonActual = pokemon;
+            mostrarPokemon(pokemon);
+            console.log(pokemon);
+        }
     };
 
-    // Endpoint de la API y método que se va a usar para llamar
     xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/" + id, true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    // Si quisieramos mandar parámetros a nuestra API, podríamos hacerlo desde el método send()
+
     xhttp.send(null);
 }
 
-function mostrar(objeto){
-    let campoNombre = document.getElementById("nombre");
-    campoNombre.innerText = "Nombre: " + objeto.name;
-
-    let campoTamano = document.getElementById("tamano");
-    campoTamano.innerText = "Estatura: " + objeto.height;
-
-    let campoExperiencia = document.getElementById("experiencia");
-    campoExperiencia.innerText = "Experiencia base: " + objeto.base_experience;
-
-    let campoIdentificador = document.getElementById("identificador");
-    campoIdentificador.innerText = "Id: " + objeto.id;
-
-    let campoPeso = document.getElementById("peso");
-    campoPeso.innerText = "Peso: " + objeto.peso;
+function mostrarPokemon(pokemon){
+    
+    mostrarMarcoDetalles();
+    mostrarImagen(pokemon);
+    mostrarNombre(pokemon);
+    mostrarAltura(pokemon);
+    mostrarPeso(pokemon);
+    mostrarExperiencia(pokemon);
+    mostrarHabilidades(pokemon);
 }
 
-function mostrarDetalles(){
-    let campoId = document.getElementById("identificador");
-    let campoPeso = document.getElementById("peso");
-    let botonMostrar = document.getElementById("botonMostrarDetalles");
-    let botonOcultar = document.getElementById("botonOcultarDetalles");
-
-    campoId.style.display = "block";
-    campoPeso.style.display = "block";
-    botonMostrar.style.display = "none";
-    botonOcultar.style.display = "block";
+function mostrarMarcoDetalles(){
+    document.getElementById("detallesPokemon").style.display = "block";    
 }
 
-function ocultarDetalles(){
-    let campoId = document.getElementById("identificador");
-    let campoPeso = document.getElementById("peso");
-    let botonMostrar = document.getElementById("botonMostrarDetalles");
-    let botonOcultar = document.getElementById("botonOcultarDetalles");
+function mostrarImagen(pokemon){
+    document.getElementById("imagen").src = pokemon.sprites.front_default;
+    document.getElementById("imagenDetalles").src = pokemon.sprites.front_default;
+}
 
-    campoId.style.display = "none";
-    campoPeso.style.display = "none";
-    botonMostrar.style.display = "block";
-    botonOcultar.style.display = "none";
+function mostrarNombre(pokemon){
+    document.getElementById("nombre").innerText = primeraLetraMayuscula(pokemon.name);
+    document.getElementById("nombreDetalles").innerText = primeraLetraMayuscula(pokemon.name);
+}
+
+function mostrarAltura(pokemon){
+    document.getElementById("altura").innerText = pokemon.height;
+}
+
+function mostrarPeso(pokemon){
+    document.getElementById("peso").innerText = pokemon.weight;
+}
+
+function mostrarExperiencia(pokemon){
+    document.getElementById("experiencia").innerText = pokemon.base_experience;
+}
+
+function mostrarHabilidades(pokemon){
+    let tabla = document.getElementById("habilidades");
+    tabla.replaceChildren();
+    
+    let longitud = pokemon.abilities.length;
+    for(let i = 0; i < longitud; i++){
+        let fila = document.createElement("tr");
+        
+        let celda = document.createElement("td");
+        celda.className = "table-primary";
+        celda.innerText = primeraLetraMayuscula(pokemon.abilities[i].ability.name);        
+
+        fila.appendChild(celda);
+        tabla.appendChild(fila);
+    }
 }
 
 function agregarAFavoritos(){
-    let tabla = document.getElementById()
+    document.getElementById("marcoFavoritos").style.display = "block";
+
+    let tabla = document.getElementById("favoritos");
+
+    let fila = document.createElement("tr");
+        
+    let celdaId = document.createElement("td");
+    celdaId.className = "table-primary";
+    celdaId.innerText = pokemonActual.id;
+    fila.appendChild(celdaId);
+
+    let celdaNombre = document.createElement("td");
+    celdaNombre.className = "table-primary";
+    celdaNombre.innerText = primeraLetraMayuscula(pokemonActual.name);
+    fila.appendChild(celdaNombre);
+
+    let celdaImagen = document.createElement("td");
+    celdaImagen.className = "table-primary";
+    celdaImagen.innerHTML = "<img src='" + pokemonActual.sprites.front_default + "'>";
+    fila.appendChild(celdaImagen);
+
+    tabla.appendChild(fila);
+}
+
+function primeraLetraMayuscula(cadena) {
+    return cadena.charAt(0).toUpperCase() + cadena.slice(1);
 }
